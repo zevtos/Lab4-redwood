3e3e045249feaadbf273c158a6e04723f9038382
Zevtos <409315@edu.itmo.ru>
2024-12-05 12:47:11 +0300

    routes / to login pages

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -31,6 +31,7 @@ const Routes = () => {
       </Set>
       <Route path="/home" page={HomePage} name="home" />
       <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/" page={LoginPage} name="login" />
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
